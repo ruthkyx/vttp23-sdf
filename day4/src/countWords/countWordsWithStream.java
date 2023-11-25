993ea8742f8787df128f86c86c69305abe194ec6
your_name <ruth.kyx@gmail.com>
2023-11-25 20:25:05 +0800

    amended count type from int to long; able to run

--- day4/src/countWords/countWordsWithStream.java
+++ day4/src/countWords/countWordsWithStream.java
@@ -8,10 +8,10 @@ import java.util.stream.Stream;
 public class countWordsWithStream {
     public static void main(String[] args) throws Exception {
         String path = "/Users/ruthie/Desktop/java/vttp23/SDF/day4/road_not_taken.txt/";
-        int count = 0;
+        long count = 0;
 
             BufferedReader br = new BufferedReader(new FileReader(path));
-            br.lines()
+            count = br.lines()
                 // clean the liune, replace all punctuations & trailing whitespaces
                 .map (line -> line.trim().replaceAll("[^\\sa-zA-Z0-9]", ""))
                 // further clean up by removing empty lines. filtering out what we want: lines that are not empty
@@ -24,7 +24,7 @@ public class countWordsWithStream {
                 //.map (words -> words.length);
 
             
-            System.out.printf("Number of words: " + count);
+            System.out.println("Number of words: " + count);
             br.close();   
 
     }
